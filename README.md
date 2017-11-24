6d678fac9bbc20b10c26e41dbea7191f8f7bda45
Liu Xiang <lxmtlab@gmail.com>
2017-11-24 14:01:29 +0800

    done ftrl

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # scalaML
 
 Implement some ML algorithms in scala. 
-Now have done BGD,SGD,MBGD with l2 regularization.
+
+1.  base bgd, sgd, mbgd to get started.
+2.  Frequently-used SGD algos： 
+    -   SGD with momentum and NAG
+    -   adagrad
+    -   adadelta
+    -   rmsprop
+    -   adam
+    -   FTRL_Proximal(online learning)
+3.  common loss function.
+
+
+**note**: 
+1.  this is just tested for binary classification(especially logloss) now. 
--- src/main/scala/com/lx/algos/optim/FTRL_Proximal.scala
+++ src/main/scala/com/lx/algos/optim/FTRL_Proximal.scala
@@ -36,24 +36,18 @@ class FTRL_Proximal(feature_size: Int) extends Optimizer with Param {
     setParams(Seq(
       "verbose" -> false, //打印日志
       "printPeriod" -> 100,
-      "iterNum" -> 5000
+      "iterNum" -> 1000,
+      "alpha" -> 0.005, //learning rate
+      "beta" -> 1.0, //smoothing parameter for adaptive learning rate
+      "lambda1" -> 0.1, //L1 regularization, larger value means more regularized
+      "lambda2" -> 5.0 //L2 regularization, larger value means more regularized
     ))
   }
 
   init_param()
 
 
-  def loss = getParam[LossFunction]("loss")
-
-  def set_loss(lossFunction: LossFunction) = setParam[LossFunction]("loss", lossFunction)
-
-
-  val alpha = 0.001 //learning rate
-  val beta = 1.0 //smoothing parameter for adaptive learning rate
-  val L1 = 0 //L1 regularization, larger value means more regularized
-  val L2 = 0 //L2 regularization, larger value means more regularized
-
-  var D = feature_size + 1// number of weights to use
+  var D = feature_size + 1 // number of weights to use
 
   var interaction = false
   //squared sum of past gradients
@@ -61,65 +55,74 @@ class FTRL_Proximal(feature_size: Int) extends Optimizer with Param {
   //adaptive lr
   var Z: DenseVector[Double] = DenseVector.rand[Double](D)
   //weights
-  var W = _weight
 
   def update(x: DenseVector[Double], p: Double, y: Double): FTRL_Proximal = {
 
-    val grad = loss.loss(p, y) * x
+    val grad = loss.dLoss(p, y) * x
 
     val square_grad = grad *:* grad
     val sigma = (sqrt(N + square_grad) - sqrt(N)) / alpha
     N += square_grad
-    Z += grad - sigma *:* W
+    Z += grad - sigma *:* _weight
     this
   }
 
-  def predict(x: DenseVector[Double]): Double = {
+
+  def loss = getParam[LossFunction]("loss")
+
+  def set_loss(lossFunction: LossFunction) = setParam[LossFunction]("loss", lossFunction)
+
+  def alpha = getParam[Double]("alpha")
+
+  def set_alpha(alpha: Double) = setParam[Double]("alpha", alpha)
+
+  def beta = getParam[Double]("beta")
+
+  def set_beta(alpha: Double) = setParam[Double]("beta", beta)
+
+  def lambda1 = getParam[Double]("lambda1")
+
+  def set_lambda1(lambda1: Double) = setParam[Double]("lambda1", lambda1)
+
+  def lambda2 = getParam[Double]("lambda2")
+
+  def set_lambda2(lambda2: Double) = setParam[Double]("lambda2", lambda2)
+
+  def iterNum = getParam[Int]("iterNum")
+
+  def set_iterNum(iterNum: Int) = setParam[Int]("iterNum", iterNum)
+
+  def verbose = getParam[Boolean]("verbose")
+
+  def set_verbose(verbose: Boolean) = setParam[Boolean]("verbose", verbose)
+
+  def printPeriod = getParam[Int]("printPeriod")
+
+  def set_printPeriod(printPeriod: Int) = setParam[Int]("printPeriod", printPeriod)
+
+
+  //仅用于训练过程中的预测
+  private def _predict(x: DenseVector[Double]): Double = {
     // wTx is the inner product of w and x
     var wTx = 0.0
 
+    var new_W = DenseVector.zeros[Double](D)
     for (i <- 0 until x.length) {
-      val sign = if(Z(i) < 0) -1.0 else  1.0
+      val sign = if (Z(i) < 0) -1.0 else 1.0
 
       /** build w on the fly using z and n, hence the name - lazy weights
         * we are doing this at prediction instead of update time is because
         * this allows us for not storing the complete
         */
-      W(i) = if (sign * Z(i) <= L1) 0.0
-      else (sign * L1 - Z(i)) / ((beta + Math.sqrt(N(i))) / alpha + L2)
+      new_W(i) = if (sign * Z(i) <= lambda1) 0.0
+      else (sign * lambda1 - Z(i)) / ((beta + Math.sqrt(N(i))) / alpha + lambda2)
 
-      wTx += W(i) * x(i)
+      wTx += new_W(i) * x(i)
     }
 
-//    1./(1 + Math.exp(-Math.max(Math.min(wTx, 35), -35)))
+    _weight = new_W
     wTx
   }
-
-
-//  def alpha = getParam[Double]("alpha")
-//
-//  def beta = getParam[Double]("beta")
-//
-//  def lambda1 = getParam[Double]("lambda1")
-//
-//  def lambda2 = getParam[Double]("lambda2")
-
-  def iterNum = getParam[Int]("iterNum")
-  def set_iterNum(iterNum: Int) = setParam[Int]("iterNum", iterNum)
-
-  def verbose = getParam[Boolean]("verbose")
-  def set_verbose(verbose: Boolean) = setParam[Boolean]("verbose", verbose)
-
-  def printPeriod = getParam[Int]("printPeriod")
-  def set_printPeriod(printPeriod: Int) = setParam[Int]("printPeriod", printPeriod)
-
-//  def set_alpha(alpha: Double) = setParam[Double]("alpha", alpha)
-//
-//  def set_beta(alpha: Double) = setParam[Double]("beta", beta)
-//
-//  def set_lambda1(lambda1: Double) = setParam[Double]("lambda1", lambda1)
-//
-//  def set_lambda2(lambda2: Double) = setParam[Double]("lambda2", lambda2)
 
 
   override def fit(X: Matrix[Double], y: Seq[Double]) = {
@@ -127,19 +130,16 @@ class FTRL_Proximal(feature_size: Int) extends Optimizer with Param {
 
     weight_init(X.cols)
     val x = input(X).toDenseMatrix
-    W = _weight
 
     breakable {
-      for (epoch <- 1 to 100) {
+      for (epoch <- 1 to iterNum) {
 
         var totalLoss: Double = 0
 
         for (i <- 0 until X.rows) {
           val ele = x(i, ::).t
-          val y_pred: Double = predict(ele)
+          val y_pred: Double = _predict(ele)
           val y_format = if (y(i) == 1.0) 1.0 else -1.0 //需要注意，分类损失函数的格式化为-1和1
-
-//          println(y_pred, y_format)
 
           update(ele, y_pred, y_format)
           totalLoss += loss.loss(y_pred, y_format)
